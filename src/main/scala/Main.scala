f4f6d276c3bb7623a1fb9e324b87ca9fd2768efa
Rafo044 <rafo044@protonmail.com>
2025-11-24 22:33:32 +0400

    İnitilization repo ..

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+@main def hello(): Unit =
+  println("Hello world!")
+  println(msg)
+
+def msg = "I was compiled by Scala 3. :)"
+
+def greet(name: Array[String]): Unit = {
+  println("Hello How are you")
+}
